39f1d04aee6130b6a1b874e4fcce4a01b0dd7b7f
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-11 12:04:44 -0300

    random number algorithms

---
+++ src/main/java/org/algorithms/intermediary/GuessANumber.java
@@ -0,0 +1,21 @@
+package org.algorithms.intermediary;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessANumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int min = 1;
+        int max = 50;
+        int number = in.nextInt();
+
+        int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+
+        if(number == random_int){
+            System.out.println("congratulations this is the number: " + random_int);
+        } else {
+            System.out.println("Number incorrect");
+        }
+    }
+}
