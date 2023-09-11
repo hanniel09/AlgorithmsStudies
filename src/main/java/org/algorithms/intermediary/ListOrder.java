47804334f40780d098e7ca99f37a091fdd3c6628
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-11 10:03:22 -0300

    ListOrder algorithms

---
+++ src/main/java/org/algorithms/intermediary/ListOrder.java
@@ -0,0 +1,14 @@
+package org.algorithms.intermediary;
+
+import java.util.Arrays;
+
+public class ListOrder {
+    public static void main(String[] args) {
+        int[] arr = {10, 12, 3, 4, 15};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
