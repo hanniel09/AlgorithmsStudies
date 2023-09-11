aae211c526ef7ef0bcc16f42d8945c3dd63287b6
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-11 10:11:04 -0300

    ListOrder algorithms

--- src/main/java/org/algorithms/intermediary/ListOrder.java
+++ src/main/java/org/algorithms/intermediary/ListOrder.java
@@ -1,10 +1,17 @@
 package org.algorithms.intermediary;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ListOrder {
     public static void main(String[] args) {
-        int[] arr = {10, 12, 3, 4, 15};
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int e = in.nextInt();
+        int[] arr = {a, b, c, d, e};
 
         Arrays.sort(arr);
 
