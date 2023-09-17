52e4b3e9b22eb3c6296230d756d7e53724615bbc
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-17 17:40:26 -0300

    new studies algorithms.

---
+++ src/main/java/org/algorithms/dataStructures/LinkedListStudy.java
@@ -0,0 +1,13 @@
+package org.algorithms.dataStructures;
+
+import java.util.LinkedList;
+
+public class LinkedListStudy {
+    public static void main(String[] args) {
+        LinkedList<String> names = new LinkedList<String>();
+        names.add("Hanniel");
+        names.add("Pedro");
+        names.add("João");
+        System.out.println(names);
+    }
+}
