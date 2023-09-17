cdeee7601f18945ca2fdc8678a0d0e1943064618
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-17 17:35:48 -0300

    new studies algorithms.

--- src/main/java/org/algorithms/collections/ArrayListPerformance.java
+++ src/main/java/org/algorithms/collections/ArrayListPerformance.java
@@ -1,7 +1,16 @@
 package org.algorithms.collections;
 
+import java.util.ArrayList;
+
 public class ArrayListPerformance {
     public static void main(String[] args) {
-
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(20);
+        numbers.add(50);
+        numbers.add(10);
+        numbers.add(3);
+        numbers.add(102);
+        numbers.sort((a, b) -> a - b);
+        System.out.println(numbers);
     }
 }
