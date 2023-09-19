2e3d8d52a15bab257b76fe8d2146af2bf876ea54
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-09-19 10:13:46 -0300

    new studies algorithms.

---
+++ src/main/java/org/algorithms/basic/StringLength.java
@@ -0,0 +1,11 @@
+package org.algorithms.basic;
+
+import java.util.Scanner;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String s = "Hello";
+        int num = s.length();
+        System.out.println(num);
+    }
+}
